88d782502d1a164c6f3ae60eea4517894e9d497a
Fabio5271 <89141452+Fabio5271@users.noreply.github.com>
2024-10-03 11:03:27 -0300

    Add Peceived Temp / Humidity and more backgrounds

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
   :class="{'freezing': typeof weather.main != 'undefined' && weather.main.temp <= 0,
           'cold': typeof weather.main != 'undefined' && weather.main.temp > 0 && weather.main.temp <= 10,
           'semi-cold': typeof weather.main != 'undefined' && weather.main.temp > 10 && weather.main.temp <= 15,
-          'midtemp': typeof weather.main == 'undefined' || (typeof weather.main != 'undefined' && weather.main.temp > 15 && weather.main.temp <= 20),
-          'semi-warm': typeof weather.main != 'undefined' && weather.main.temp > 20 && weather.main.temp <= 26,
-          'warm': typeof weather.main != 'undefined' && weather.main.temp > 26 && weather.main.temp <= 30,
-          'hot': typeof weather.main != 'undefined' && weather.main.temp > 30}">
+          'sl-cold': typeof weather.main != 'undefined' && weather.main.temp > 15 && weather.main.temp <= 18,
+          'midtemp': typeof weather.main != 'undefined' && weather.main.temp > 18 && weather.main.temp <= 22,
+          'sl-warm': typeof weather.main != 'undefined' && weather.main.temp > 22 && weather.main.temp <= 25,
+          'semi-warm': typeof weather.main != 'undefined' && weather.main.temp > 25 && weather.main.temp <= 28,
+          'warm': typeof weather.main != 'undefined' && weather.main.temp > 28 && weather.main.temp <= 32,
+          'hot': typeof weather.main != 'undefined' && weather.main.temp > 32,
+          'undef': typeof weather.main == 'undefined'}">
     <main>
       <div class="search-box">
           <input
             type="text"
             class="search-bar"
-            placeholder="Search..."
+            placeholder="Search...  (City name, Country code)"
             v-model="query"
             @keypress="fetchWeather"/>
       </div>
@@ -28,6 +31,13 @@
         <div class="weather-box">
           <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+        
+        <div class="weather-spacer"></div>
+
+        <div class="weather-extras">
+          <div class="feelslike"><span class="flt">Feels Like</span><br><span class="fltemp">{{ Math.round(weather.main.feels_like) }}°C</span></div>
+          <div class="humidity"><span class="flt">Humidity</span><br><span class="humval">{{ weather.main.humidity }}%</span></div>
         </div>
       </div>
     </main>
@@ -104,31 +114,43 @@ body {
 }
 
 #app.freezing {
-  background-image: url('./assets/backgrounds/freezing-bg.jpg');
+  background-image: url('./assets/backgrounds/freezing.jpg');
 }
 
 #app.cold {
-  background-image: url('./assets/backgrounds/cold-bg.jpg');
+  background-image: url('./assets/backgrounds/cold.jpg');
 }
 
 #app.semi-cold {
-  background-image: url('./assets/backgrounds/semi-cold-bg.jpg');
+  background-image: url('./assets/backgrounds/semi-cold.jpg');
+}
+
+#app.sl-cold {
+  background-image: url('./assets/backgrounds/sl-cold.jpg');
 }
 
 #app.midtemp {
-  background-image: url('./assets/backgrounds/midtemp-bg.png');
+  background-image: url('./assets/backgrounds/midtemp.png');
+}
+
+#app.sl-warm {
+  background-image: url('./assets/backgrounds/sl-warm.jpg');
 }
 
 #app.semi-warm {
-  background-image: url('./assets/backgrounds/semi-warm-bg.jpg');
+  background-image: url('./assets/backgrounds/semi-warm.jpg');
 }
 
 #app.warm {
-  background-image: url('./assets/backgrounds/warm-bg.jpg');
+  background-image: url('./assets/backgrounds/warm.jpg');
 }
 
 #app.hot {
-  background-image: url('./assets/backgrounds/hot-bg.jpg');
+  background-image: url('./assets/backgrounds/hot.jpg');
+}
+
+#app.undef {
+  background-image: url('./assets/backgrounds/undef.png');
 }
 
 main {
@@ -184,6 +206,15 @@ main {
 
 .weather-box {
   text-align: center;
+  margin-bottom:5px ;
+}
+
+.weather-spacer {
+  height: 0px;
+  border-bottom: 1px solid #fff;
+  box-shadow: 0px 2px rgba(0, 0, 0, 0.25);
+  margin: 5px auto;
+  width: 75%;
 }
 
 .weather-box .temp {
@@ -207,5 +238,40 @@ main {
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  margin-bottom: 15px;
 }
+
+.weather-extras {
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+  margin-top: 15px;
+}
+
+.weather-extras .feelslike, .weather-extras .humidity {
+  display: inline-block;
+  padding: 5px 12px;
+  color: #fff;
+  font-size: 24px;
+  font-weight: 500;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 8px;
+  margin:  15px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.feelslike .fltemp, .humidity .humval {
+  font-size: 50px;
+  font-weight: 900;
+}
+/* 
+.weather-extras .feelslike .flt {
+  font-size: 24px;
+  font-weight: 500;
+} */
+
+
 </style>
