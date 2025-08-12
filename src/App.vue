a56f316a8a514cd5a0a70d0c77c6c9e86cc5a0ff
Fabio5271 <89141452+Fabio5271@users.noreply.github.com>
2025-08-12 17:42:15 -0300

    Transform extra widgets into a generic, reusable component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div id="app"
-  :class="{'freezing': typeof weather.main != 'undefined' && weather.main.temp <= 0,
-          'cold': typeof weather.main != 'undefined' && weather.main.temp > 0 && weather.main.temp <= 10,
-          'semi-cold': typeof weather.main != 'undefined' && weather.main.temp > 10 && weather.main.temp <= 15,
-          'sl-cold': typeof weather.main != 'undefined' && weather.main.temp > 15 && weather.main.temp <= 18,
-          'midtemp': typeof weather.main != 'undefined' && weather.main.temp > 18 && weather.main.temp <= 22,
-          'sl-warm': typeof weather.main != 'undefined' && weather.main.temp > 22 && weather.main.temp <= 25,
-          'semi-warm': typeof weather.main != 'undefined' && weather.main.temp > 25 && weather.main.temp <= 28,
-          'warm': typeof weather.main != 'undefined' && weather.main.temp > 28 && weather.main.temp <= 32,
-          'hot': typeof weather.main != 'undefined' && weather.main.temp > 32,
-          'undef': typeof weather.main == 'undefined'}">
+  <div id="app" :class="{
+    'freezing': typeof weather.main != 'undefined' && weather.main.temp <= 0,
+    'cold': typeof weather.main != 'undefined' && weather.main.temp > 0 && weather.main.temp <= 10,
+    'semi-cold': typeof weather.main != 'undefined' && weather.main.temp > 10 && weather.main.temp <= 15,
+    'sl-cold': typeof weather.main != 'undefined' && weather.main.temp > 15 && weather.main.temp <= 18,
+    'midtemp': typeof weather.main != 'undefined' && weather.main.temp > 18 && weather.main.temp <= 22,
+    'sl-warm': typeof weather.main != 'undefined' && weather.main.temp > 22 && weather.main.temp <= 25,
+    'semi-warm': typeof weather.main != 'undefined' && weather.main.temp > 25 && weather.main.temp <= 28,
+    'warm': typeof weather.main != 'undefined' && weather.main.temp > 28 && weather.main.temp <= 32,
+    'hot': typeof weather.main != 'undefined' && weather.main.temp > 32,
+    'undef': typeof weather.main == 'undefined'
+  }">
     <main>
       <div class="search-box">
-          <input
-            type="text"
-            class="search-bar"
-            placeholder="Search...  (City name, Country code)"
-            v-model="query"
-            @keypress="fetchWeather"/>
+        <input type="text" class="search-bar" placeholder="Search...  (City name, Country code)" v-model="query"
+          @keypress="fetchWeather" />
       </div>
 
       <div class="results-container" v-if="typeof weather.main != 'undefined'">
@@ -30,12 +27,12 @@
           <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
-        
+
         <div class="weather-spacer"></div>
 
         <div class="weather-extras">
-          <div class="extra-widget feelslike"><span class="flt">Feels Like</span><br><span class="value fltemp">{{ Math.round(weather.main.feels_like) }}°C</span></div>
-          <div class="extra-widget humidity"><span class="flt">Humidity</span><br><span class="value humval">{{ weather.main.humidity }}%</span></div>
+          <ExtraWidget title='Feels Like' :val="formattedData.feels_like" unit='°C' />
+          <ExtraWidget title='Humidity' :val="weather.main.humidity" unit='%' />
         </div>
       </div>
     </main>
@@ -45,28 +42,36 @@
 
 
 <script>
+import ExtraWidget from './components/ExtraWidget.vue';
+
 export default {
   name: 'App',
-  data () {
+  components: {
+    ExtraWidget
+  },
+  data() {
     return {
       url_base: 'https://weatherapp-backend-fawn.vercel.app/api/',
       query: '',
-      weather: {}
+      weather: {},
+      formattedData: {}
     }
   },
   methods: {
-    fetchWeather (e) {
+    fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(`${this.url_base}weather?q=${this.query}`)
-        .then(res => {
-          return res.json();
-        }).then(this.setResults);
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
       }
     },
-    setResults (results) {
+    setResults(results) {
       this.weather = results;
+      this.formattedData.feels_like = Math.round(results.main.feels_like);
+      this.formattedData.localtime = this.dateBuilder("localtime");
     },
-    dateBuilder () {
+    dateBuilder(query) {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -75,8 +80,15 @@ export default {
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
+      let localtime = d.getHours() + ":" + d.getMinutes()
 
-      return `${day} ${date} ${month} ${year}`;
+      switch (query) {
+        case "localtime":
+          return `${localtime}`;
+      
+        default:
+          return `${day} ${date} ${month} ${year}`;
+      }
     }
   }
 }
@@ -99,46 +111,6 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
-}
-
-#app.freezing {
-  background-image: url('./assets/backgrounds/freezing.jpg');
-}
-
-#app.cold {
-  background-image: url('./assets/backgrounds/cold.jpg');
-}
-
-#app.semi-cold {
-  background-image: url('./assets/backgrounds/semi-cold.jpg');
-}
-
-#app.sl-cold {
-  background-image: url('./assets/backgrounds/sl-cold.jpg');
-}
-
-#app.midtemp {
-  background-image: url('./assets/backgrounds/midtemp.png');
-}
-
-#app.sl-warm {
-  background-image: url('./assets/backgrounds/sl-warm.jpg');
-}
-
-#app.semi-warm {
-  background-image: url('./assets/backgrounds/semi-warm.jpg');
-}
-
-#app.warm {
-  background-image: url('./assets/backgrounds/warm.jpg');
-}
-
-#app.hot {
-  background-image: url('./assets/backgrounds/hot.jpg');
-}
-
-#app.undef {
-  background-image: url('./assets/backgrounds/undef.png');
 }
 
 main {
@@ -245,11 +217,11 @@ main {
   justify-content: space-around;
   width: 75%;
 }
-
-.weather-extras .extra-widget{
+/* 
+.weather-extras .extra-widget {
   display: inline-block;
   padding: 5px 12px;
-  margin:  15px 15px;
+  margin: 15px 15px;
   text-align: center;
   font-size: 24px;
   font-weight: 500;
@@ -263,5 +235,45 @@ main {
 .weather-extras .extra-widget .value {
   font-size: 50px;
   font-weight: 900;
+} */
+
+#app.freezing {
+  background-image: url('./assets/backgrounds/freezing.jpg');
+}
+
+#app.cold {
+  background-image: url('./assets/backgrounds/cold.jpg');
+}
+
+#app.semi-cold {
+  background-image: url('./assets/backgrounds/semi-cold.jpg');
+}
+
+#app.sl-cold {
+  background-image: url('./assets/backgrounds/sl-cold.jpg');
+}
+
+#app.midtemp {
+  background-image: url('./assets/backgrounds/midtemp.png');
+}
+
+#app.sl-warm {
+  background-image: url('./assets/backgrounds/sl-warm.jpg');
+}
+
+#app.semi-warm {
+  background-image: url('./assets/backgrounds/semi-warm.jpg');
+}
+
+#app.warm {
+  background-image: url('./assets/backgrounds/warm.jpg');
+}
+
+#app.hot {
+  background-image: url('./assets/backgrounds/hot.jpg');
+}
+
+#app.undef {
+  background-image: url('./assets/backgrounds/undef.png');
 }
 </style>
---
+++ src/components/ExtraWidget.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="extra-widget">
+    <span class="title">{{ title }}</span><br>
+    <span class="value">{{ val }}{{ unit }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExtraWidget',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    val: {
+      type: String,
+      required: true
+    },
+    unit: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.extra-widget {
+  display: inline-block;
+  padding: 5px 12px;
+  margin: 15px 15px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  color: #fff;
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 8px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.extra-widget .value {
+  font-size: 50px;
+  font-weight: 900;
+}
+</style>
